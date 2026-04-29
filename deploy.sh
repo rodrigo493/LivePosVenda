241611f2f77844c7536223409b30eb5d0a3a0c53
Rodrigo Siqueira <rodrigo@liveequipamentos.com.br>
2026-04-28 21:57:28 -0300

    chore: atualiza deploy.sh com git pull e sem --no-cache

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
-docker build --no-cache \
+cd /opt/posvenda
+git pull
+docker build \
   --build-arg VITE_SUPABASE_URL=https://ehqkggiuouczmafmlzls.supabase.co \
   --build-arg VITE_SUPABASE_PUBLISHABLE_KEY=sb_publishable_TBXHRRXXZEok0JWK08lbeQ_tcJdv4gZ \
-  -t posvenda:latest /opt/posvenda
+  -t posvenda:latest .
 docker service update --image posvenda:latest --force posvenda_posvenda
 docker service ps posvenda_posvenda
+echo "Deploy concluido!"
